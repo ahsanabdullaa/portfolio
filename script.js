8351d2bd2b077ba2cacf170717f06dae558e653e
ahsanabdullaa <73553412+ahsanabdullaa@users.noreply.github.com>
2022-03-21 17:20:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.getElementById("button").addEventListener("click", function(){
 document.querySelector(".close").addEventListener("click", function(){
     document.querySelector(".popup").style.display = "none";
 })
+
+var $root = $('html, body');
+
+$('a[href^="#"]').click(function () {
+    $root.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+
+    return false;
+});
